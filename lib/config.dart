61c3ca1385f574f72c696ee32411942cb7f10140
FordenRo <FordenRo@yandex.ru>
2026-05-04 15:35:52 +0500

    Improved config, now saving a lot more data

--- lib/config.dart
+++ lib/config.dart
@@ -15,17 +15,41 @@ Future<void> loadConfiguration() async {
   var json = jsonDecode(await file.readAsString());
 
   importedTracks = await Future.wait(
-    (json['imported'] as List).cast<String>().map(AudioTrack.fromPath),
+    (json['imported'] as List?)?.cast<String>().map(AudioTrack.fromPath) ?? [],
   );
-  await audioPlayer.setVolume(json['volume']);
-  await Future.microtask(
-    () => audioPlayer.setAudioDevice(
+
+  var queue =
+      (json['queue'] as List?)
+          ?.cast<String>()
+          .map(
+            (path) =>
+                importedTracks.where((track) => track.path == path).firstOrNull,
+          )
+          .nonNulls
+          .toList() ??
+      [];
+  if (queue.isNotEmpty) {
+    await audioPlayer.setQueue(queue, index: json['index'] as int? ?? 0);
+  }
+
+  await audioPlayer.setVolume(json['volume'] ?? 1);
+  audioPlayer.setLooped(json['lp'] as bool? ?? false);
+  audioPlayer.setShuffled(json['sh'] as bool? ?? false);
+
+  await Future.microtask(() async {
+    await audioPlayer.setAudioDevice(
       audioPlayer.audioDevices.firstWhere(
-        (e) => e.name == json['device'] as String,
+        (e) => e.name == json['device'] as String?,
         orElse: () => audioPlayer.audioDevice,
       ),
-    ),
-  );
+    );
+  });
+
+  Future.delayed(const Duration(milliseconds: 100), () {
+    if (queue.isNotEmpty) {
+      return audioPlayer.seek(Duration(milliseconds: json['pos'] as int? ?? 0));
+    }
+  });
 }
 
 Future<void> saveConfiguration() async {
@@ -37,6 +61,11 @@ Future<void> saveConfiguration() async {
       'device': audioPlayer.audioDevice.name,
       'volume': audioPlayer.volume,
       'imported': importedTracks.map((e) => e.path).toList(),
+      'queue': audioPlayer.queue.map((e) => e.path).toList(),
+      'index': audioPlayer.currentIndex,
+      'pos': audioPlayer.position.inMilliseconds,
+      'lp': audioPlayer.looped,
+      'sh': audioPlayer.shuffled,
     }),
   );
 }
