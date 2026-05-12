df19f9dd150fc7df4c73b27752fb642a1a3776e7
FordenRo <FordenRo@yandex.ru>
2026-05-12 16:36:07 +0500

    Added deletion settings

--- lib/features/settings_page.dart
+++ lib/features/settings_page.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../core/config.dart';
+
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => ListView(
+    children: [
+      ListTile(
+        title: const Text('Delete all playlists'),
+        trailing: IconButton(
+          onPressed: playlists.clear,
+          icon: const Icon(Icons.delete_outlined),
+        ),
+      ),
+      ListTile(
+        title: const Text('Delete all imports'),
+        trailing: IconButton(
+          onPressed: importedPlaylist.tracks.clear,
+          icon: const Icon(Icons.delete_outlined),
+        ),
+      ),
+    ],
+  );
 }
