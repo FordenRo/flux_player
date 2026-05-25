61450948c302cd198a5e7278b7987b341e1bcb4c
FordenRo <FordenRo@yandex.ru>
2026-05-25 20:19:17 +0500

    Changing playback position via scrolling Optimized position slider

--- lib/widgets/player_controls/widgets/position_slider.dart
+++ lib/widgets/player_controls/widgets/position_slider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' show Vector3;
 import '../../../core/audio_player/audio_player.dart';
@@ -24,12 +25,10 @@ class _PositionSliderState extends State<PositionSlider> {
   @override
   void initState() {
     super.initState();
-
     stream = .broadcast()
       ..addStream(
         audioPlayer.stream.position,
-      ).then((_) => stream.addStream(audioPlayer.stream.duration))
-      ..stream.listen((_) => setState(() {}));
+      ).then((_) => stream.addStream(audioPlayer.stream.duration));
   }
 
   @override
@@ -40,32 +39,46 @@ class _PositionSliderState extends State<PositionSlider> {
 
   @override
   Widget build(BuildContext context) => MouseRegion(
-    onEnter: (_) => setState(() => hovered = true),
-    onExit: (_) => setState(() => hovered = false),
-    child: AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      transform: Matrix4.translation(Vector3(0, expanded ? 0 : 3, 0)),
-      height: expanded ? 10 : 8,
-      child: SliderTheme(
-        data: SliderThemeData(
-          trackHeight: 8,
-          padding: .zero,
-          activeTrackColor: Theme.of(
-            context,
-          ).colorScheme.primary.withAlpha(200),
-          thumbColor: Theme.of(context).colorScheme.primary,
-          thumbShape: RoundSliderThumbShape(
-            enabledThumbRadius: expanded ? 6 : 0,
-            elevation: 4,
+    onEnter: (e) => setState(() => hovered = true),
+    onExit: (e) => setState(() => hovered = false),
+    child: Listener(
+      onPointerSignal: (e) {
+        if (e is PointerScrollEvent) {
+          audioPlayer.seek(
+            Duration(
+              milliseconds: (position * 1000 - e.scrollDelta.dy * 10).toInt(),
+            ),
+          );
+        }
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        transform: Matrix4.translation(Vector3(0, expanded ? 0 : 3, 0)),
+        height: expanded ? 10 : 8,
+        child: SliderTheme(
+          data: SliderThemeData(
+            trackHeight: 8,
+            padding: .zero,
+            activeTrackColor: Theme.of(
+              context,
+            ).colorScheme.primary.withAlpha(200),
+            thumbColor: Theme.of(context).colorScheme.primary,
+            thumbShape: RoundSliderThumbShape(
+              enabledThumbRadius: expanded ? 6 : 0,
+              elevation: 4,
+            ),
+            overlayShape: const RoundSliderOverlayShape(overlayRadius: 12),
           ),
-          overlayShape: const RoundSliderOverlayShape(overlayRadius: 12),
-        ),
-        child: Slider(
-          value: duration > 0 ? max(min(position / duration, 1), 0) : 0,
-          onChangeStart: (_) => setState(() => sliding = true),
-          onChangeEnd: (_) => setState(() => sliding = false),
-          onChanged: (e) => audioPlayer.seek(
-            Duration(milliseconds: (e * duration * 1000).toInt()),
+          child: StreamBuilder(
+            stream: stream.stream,
+            builder: (context, asyncSnapshot) => Slider(
+              value: duration > 0 ? max(min(position / duration, 1), 0) : 0,
+              onChangeStart: (_) => setState(() => sliding = true),
+              onChangeEnd: (_) => setState(() => sliding = false),
+              onChanged: (e) => audioPlayer.seek(
+                Duration(milliseconds: (e * duration * 1000).toInt()),
+              ),
+            ),
           ),
         ),
       ),
